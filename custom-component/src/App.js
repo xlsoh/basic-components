160186a94d3623cd144fe960c0bddf24f4db83ac
xlsoh <thgus360@gmail.com>
2022-02-09 22:27:29 +0900

    FIX: fix some code

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
-import Toggle from './component/Toggle';
-import Modal from './component/Modal';
-import './component/Tab';
-import './component/Tag';
-import './component/AutoComplete';
-import './component/ClickToEdit';
+import Toggle from './components/Toggle';
+import Modal from './components/Modal';
+import './components/Tab';
+import './components/Tag';
+import './components/AutoComplete';
+import './components/ClickToEdit';
 
 function App() {
   return (
     <AppContainer>
       <Title> Toggle</Title>
       <Toggle />
+      <br />
+      <br />
+      <Title>Modal</Title>
+      <Modal />
     </AppContainer>
   );
 }
@@ -22,17 +26,18 @@ const AppContainer = styled.div`
   position: absolute; //top0위해서
   top: 0;
   left: 0;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   display: flex; //고른 배열
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  justify-content: center; // 콘텐츠 열 중앙
+  align-items: center; //콘텐츠 행 중앙
   line-height: 2;
-  background-color: aliceblue;
+  background: linear-gradient(aliceblue, #8dc7e9);
 `;
 
 const Title = styled.div`
   font-weight: 800;
   font-size: 1.6em;
+  text-align: left;
 `;
