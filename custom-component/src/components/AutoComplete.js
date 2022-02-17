9ccc402ee8c7188fa35468e13eddbe23532f7b9c
PARKSOHYUN <thgus360@gmail.com>
2022-02-17 15:34:59 +0900

    ADD: add files

---
+++ custom-component/src/components/AutoComplete.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function AutoComplete() {
+  return <Container>Will be updated!</Container>;
+}
+
+export default AutoComplete;
+
+const Container = styled.div``;
---
+++ custom-component/src/components/ClickToEdit.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function ClickToEdit() {
+  return <Container>Will be updated!</Container>;
+}
+
+export default ClickToEdit;
+
+const Container = styled.div``;
